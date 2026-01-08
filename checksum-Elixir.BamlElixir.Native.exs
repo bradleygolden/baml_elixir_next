57c2f3519e00e9d924457865e1e08b7507d94398
Bradley Golden <golden.bradley@gmail.com>
2026-01-08 10:35:50 -0600

    Update checksums for v1.0.0-pre.23.next.1

--- checksum-Elixir.BamlElixir.Native.exs
+++ checksum-Elixir.BamlElixir.Native.exs
@@ -1,4 +1,5 @@
 %{
-  "libbaml_elixir-v1.0.0-pre.22-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:8f378fce2e43b9e4217acd1520506f76ad5b1455f2bf6ae455abeaa0f9fad20d",
-  "libbaml_elixir-v1.0.0-pre.22-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:a7a38b869b41a8eee50ea93828ef62f1fab38ba9fba7f765dbaa8e84a8ff6653",
+  "libbaml_elixir-v1.0.0-pre.23.next.1-nif-2.15-aarch64-apple-darwin.so.tar.gz" => "sha256:85cb93162e8928d48ae2e66cf986865e667785179d6c9a8d4ffa23404cca74c6",
+  "libbaml_elixir-v1.0.0-pre.23.next.1-nif-2.15-aarch64-unknown-linux-gnu.so.tar.gz" => "sha256:aec770cb3266824e4a390a1692a0ebd75496380b1a8a899faa8b4a9f600b5272",
+  "libbaml_elixir-v1.0.0-pre.23.next.1-nif-2.15-x86_64-unknown-linux-gnu.so.tar.gz" => "sha256:018fb938245b46ec8f9d992d21d8acde120d320f96be2cb42ec8ecd5bd0a8fb1",
 }
